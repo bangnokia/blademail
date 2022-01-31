7e772a0f1945bfeff5e2902d7c6179c530b6c2fe
bangnokia <bangnokia@gmail.com>
2022-01-31 11:12:56 +0700

    work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
-import { useLayoutEffect, useEffect } from "react";
+import { useEffect } from "react";
 import CurrentEmail from "./components/CurrentEmail";
 import Mailbox from "./components/Mailbox";
 import { startSmtpServer } from "./smtp";
+import { listen, Event } from "@tauri-apps/api/event";
+import useStore, { Email } from "./store";
+import { nanoid } from "nanoid";
+
+function makeExcerpt(email: Email) {
+    let excerpt = "";
+
+    if (email.html) {
+        const text = new DOMParser().parseFromString(email.html, "text/html").body.textContent?.trim();
+        excerpt = text ? text.substring(0, 120) : "";
+    } else {
+        excerpt = email.text.substring(0, 120);
+    }
+
+    return excerpt + "...";
+}
 
 function App() {
+    const [addEmail] = useStore((state) => [state.addEmail]);
     useEffect(function () {
         startSmtpServer().then(() => console.log("SMTP server started"));
+    }, []);
+
+    useEffect(function () {
+        listen("new-email", (event: Event<Email>) => {
+            const email: Email = {
+                ...event.payload,
+                id: nanoid(),
+                excerpt: makeExcerpt(event.payload),
+                date: new Date(),
+                isOpen: false,
+            };
+
+            addEmail(email);
+        });
     }, []);
 
     return (
--- src/components/Mailbox.tsx
+++ src/components/Mailbox.tsx
@@ -1,47 +1,14 @@
-import { listen, Event } from "@tauri-apps/api/event";
-import { useEffect, useState } from "react";
 import MailItem from "./MailItem";
 import useStore, { Email } from "../store";
 import shallow from "zustand/shallow";
-import { nanoid } from "nanoid";
-
-function makeExcerpt(email: Email) {
-    let excerpt = "";
-
-    if (email.html) {
-        const text = new DOMParser().parseFromString(email.html, "text/html").body.textContent?.trim();
-        excerpt = text ? text.substring(0, 120) : "";
-    } else {
-        excerpt = email.text.substring(0, 120);
-    }
-
-    return excerpt + "...";
-}
 
 export default function Mailbox() {
-    const [emails, addEmail, setCurrentEmailId] = useStore(
-        (state) => [state.emails, state.addEmail, state.setCurrentEmailId],
-        shallow
-    );
+    const [emails, setCurrentEmailId] = useStore((state) => [state.emails, state.setCurrentEmailId], shallow);
 
     function openEmail(email: Email) {
         email.isOpen = true;
         setCurrentEmailId(email.id);
     }
-
-    useEffect(function () {
-        listen("new-email", (event: Event<Email>) => {
-            const email: Email = {
-                ...event.payload,
-                id: nanoid(),
-                excerpt: makeExcerpt(event.payload),
-                date: new Date(),
-                isOpen: false,
-            };
-
-            addEmail(email);
-        });
-    }, []);
 
     return (
         <div className="flex h-full w-[450px] flex-col overflow-y-auto p-2 text-white">
