92b16a9e34befae16acf6a3c3305c52e0727fdc2
bangnokia <bangnokia@gmail.com>
2022-06-20 23:10:52 +0700

    minimal UI

--- src/components/Mailbox.tsx
+++ src/components/Mailbox.tsx
@@ -13,26 +13,32 @@ export default function Mailbox() {
     }
 
     return (
-        <div className="hidden h-full w-[322px] shrink-0 grow-0 flex-col border-r border-gray-300 text-white md:flex">
-            <div className="toolbox sticky w-full top-0 z-20 flex items-end justify-between bg-white px-2 py-1 shadow-sm">
-                <ButtonDeleteEmails />
-            </div>
-
-            <div className="items-stretch overflow-y-auto p-2">
-                {emails.map((email, index) => {
-                    return (
-                        <div onClick={() => openEmail(email)} key={email.id}>
-                            <MailItem email={email} active={false} />
+        <>
+            {
+                emails.length > 0 && (
+                    <div className="hidden h-full w-[322px] shrink-0 grow-0 flex-col border-r border-gray-300 text-white md:flex">
+                        <div className="toolbox sticky w-full top-0 z-20 flex items-end justify-between bg-white px-2 py-1 shadow-sm">
+                            <ButtonDeleteEmails />
                         </div>
-                    );
-                })}
-            </div>
 
-            {emails.length === 0 && (
-                <div className="h-full flex items-center justify-center text-gray-500 text-sm">
-                    Your mailbox is clean
-                </div>
-            )}
-        </div>
+                        <div className="items-stretch overflow-y-auto p-2">
+                            {emails.map((email, index) => {
+                                return (
+                                    <div onClick={() => openEmail(email)} key={email.id}>
+                                        <MailItem email={email} active={false} />
+                                    </div>
+                                );
+                            })}
+                        </div>
+
+                        {emails.length === 0 && (
+                            <div className="h-full flex items-center justify-center text-gray-500 text-sm">
+                                Your mailbox is clean
+                            </div>
+                        )}
+                    </div>
+                )
+            }
+        </>
     );
 }
