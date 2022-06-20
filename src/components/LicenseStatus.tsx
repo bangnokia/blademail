2fc2aede765a382fb48d215a920145d2b02440b0
bangnokia <bangnokia@gmail.com>
2022-06-20 23:05:14 +0700

    add input license key form

--- src/components/LicenseStatus.tsx
+++ src/components/LicenseStatus.tsx
@@ -1,46 +1,59 @@
 import { useEffect, useState } from "react"
 import { getLicense, saveLicense, verify } from "../stores/license"
 import { appDir } from "@tauri-apps/api/path"
+import Cog from "./icons/Cog"
 
 export default function LicenseStatus() {
   const [valid, setValid] = useState(true)
-  const [open, setOpen] = useState(true)
+  const [open, setOpen] = useState(false)
   const [license, setLicense] = useState<string>('')
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     getLicense().then(val => {
-      console.log('get license', val)
       setLicense(val === null ? '' : val)
+      if (val) {
+        verify(val).then(result => {
+          if (!result) {
+            setValid(false)
+          } else {
+            setValid(true)
+          }
+        })
+      }
     })
   }, [])
 
   async function submit(e: any) { // this shit
     e.preventDefault();
     setLoading(true)
+    let isValid = await verify(license)
+    setLoading(false)
 
-    const isValid = await verify(license)
-
-    if (!true) {
+    if (!isValid) {
       setError('Your license is invalid')
       setLoading(false)
+      setValid(false)
       return;
     }
 
     setError('')
     await saveLicense(license + '')
+    setValid(true)
     setOpen(false)
   }
 
   return (
-    <div>
-      {valid ? 'Registered' : 'Unregistered'}
-      <button onClick={() => setOpen(true)}>Enter license</button>
+    <div className="flex items-center justify-center gap-1">
+      {!valid && 'Unregistered'}
+      <button onClick={() => setOpen(true)}>
+        <Cog className="w-4 h-4" />
+      </button>
       {open && (
         <div className="fixed inset-0 w-screen h-screen z-40 bg-gray-500/40 grid place-items-center">
 
-          <form className="inset-0 bg-white flex flex-col gap-6 px-8 py-6 rounded shadow" onSubmit={submit}>
+          <form className="inset-0 bg-white flex flex-col gap-6 px-8 py-6 rounded-md shadow" onSubmit={submit}>
             <div className="flex flex-col gap-2">
               <label htmlFor="license-key" className="text-base font-medium">License key</label>
               <input type="text" id="license-key" placeholder="Your license key"
@@ -51,8 +64,8 @@ export default function LicenseStatus() {
             </div>
 
             <div className="flex items-center justify-end gap-2">
-              <button onClick={() => setOpen(false)} className="bg-gray-500 text-white px-4 py-2 rounded">Cancel</button>
-              <button type="submit" className="bg-rose-500 text-white px-4 py-2 rounded disabled:bg-opacity-50" disabled={loading}>Save</button>
+              <button onClick={() => setOpen(false)} className="bg-gray-400 text-white px-4 py-2 rounded">Cancel</button>
+              <button type="submit" className="bg-rose-500 text-white px-4 py-2 rounded disabled:bg-opacity-50" disabled={loading || !license}>Save</button>
             </div>
           </form>
 
