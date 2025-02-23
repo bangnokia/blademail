eeee0b14cf82c7e7d4fda15cd4c871c9f9cd6999
bangnokia <bangnokia@gmail.com>
2025-02-23 16:49:17 +0700

    feat: integrate Tailwind CSS with PostCSS and add utility functions for email handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +10,11 @@ export default defineConfig({
     port: 4001
   },
   plugins: [vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
