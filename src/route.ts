036d333a9a8594e129500fd29893ae8b81c59223
bangnokia <bangnokia@gmail.com>
2025-02-23 17:32:29 +0700

    🚑 fix: Remove unnecessary empty line in src/route.ts

--- src/route.ts
+++ src/route.ts
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from "vue-router";
 import Email from './views/Email.vue'
 import Home from './views/Home.vue'
 
-
 const routes = [
   { path: '/', component: Home, name: 'home' },
   { path: '/emails/:id', component: Email, name: 'emails.show', props: true },
